d990a78d5e3ebed0f9d0f5879ae0fc9906d4b8a6
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2025-02-20 00:08:26 +0100

    add asserter internal annotations

--- src/Assert/AsserterInterface.php
+++ src/Assert/AsserterInterface.php
@@ -6,6 +6,9 @@ namespace VStelmakh\TestLogger\Assert;
 
 use VStelmakh\TestLogger\Log\Collection;
 
+/**
+ * @internal
+ */
 interface AsserterInterface
 {
     public function assert(Collection $logs, ?string $criterion): void;
--- src/Assert/HasLogsAsserter.php
+++ src/Assert/HasLogsAsserter.php
@@ -6,6 +6,9 @@ namespace VStelmakh\TestLogger\Assert;
 
 use VStelmakh\TestLogger\Log\Collection;
 
+/**
+ * @internal
+ */
 class HasLogsAsserter implements AsserterInterface
 {
     /** @var array<string> */

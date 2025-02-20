f63d121297f5f31d426f373c4e5cdf54081e6b3a
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2025-02-20 21:58:36 +0100

    add assert test

---
+++ tests/Match/AssertTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VStelmakh\TestLogger\Tests\Match;
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Psr\Log\LogLevel;
+use VStelmakh\TestLogger\Log\Collection;
+use VStelmakh\TestLogger\Log\Log;
+use VStelmakh\TestLogger\Match\Assert;
+use PHPUnit\Framework\TestCase;
+
+class AssertTest extends TestCase
+{
+    #[DataProvider('hasProvider')]
+    public function testHas(string $method, ?string $level, bool $isSuccess): void
+    {
+        $logs = new Collection();
+        if ($level !== null) {
+            $logs->add(new Log($level, 'Test message.'));
+        }
+
+        $message = 'Custom assertation error message.';
+
+        if (!$isSuccess) {
+            $this->expectException(AssertionFailedError::class);
+            $this->expectExceptionMessage($message);
+        }
+
+        $assert = new Assert($logs, $message);
+        $assert->{$method}();
+    }
+
+    public static function hasProvider(): array
+    {
+        return [
+            ['hasLogs', null, false],
+            ['hasLogs', LogLevel::INFO, true],
+            ['hasDebug', LogLevel::INFO, false],
+            ['hasDebug', LogLevel::DEBUG, true],
+            ['hasInfo', LogLevel::DEBUG, false],
+            ['hasInfo', LogLevel::INFO, true],
+            ['hasNotice', LogLevel::NOTICE, true],
+            ['hasNotice', LogLevel::INFO, false],
+            ['hasWarning', LogLevel::WARNING, true],
+            ['hasWarning', LogLevel::INFO, false],
+            ['hasError', LogLevel::ERROR, true],
+            ['hasError', LogLevel::INFO, false],
+            ['hasCritical', LogLevel::CRITICAL, true],
+            ['hasCritical', LogLevel::INFO, false],
+            ['hasAlert', LogLevel::ALERT, true],
+            ['hasAlert', LogLevel::INFO, false],
+            ['hasEmergency', LogLevel::EMERGENCY, true],
+            ['hasAlert', LogLevel::INFO, false],
+        ];
+    }
+}

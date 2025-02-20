bdd4c1d153b941bf2be7496d794253c3051c3d8e
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2025-02-20 22:16:11 +0100

    add filter test

---
+++ tests/Match/FilterTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VStelmakh\TestLogger\Tests\Match;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use Psr\Log\LogLevel;
+use VStelmakh\TestLogger\Log\Collection;
+use VStelmakh\TestLogger\Log\Log;
+use VStelmakh\TestLogger\Match\Filter;
+use PHPUnit\Framework\TestCase;
+use VStelmakh\TestLogger\Match\Matcher;
+
+class FilterTest extends TestCase
+{
+    #[DataProvider('getProvider')]
+    public function testGet(string $method, array $expected): void
+    {
+        $logs = new Collection();
+        $logs->add(new Log(LogLevel::DEBUG, 'Debug'));
+        $logs->add(new Log(LogLevel::INFO, 'Info'));
+        $logs->add(new Log(LogLevel::NOTICE, 'Notice'));
+        $logs->add(new Log(LogLevel::WARNING, 'Warning'));
+        $logs->add(new Log(LogLevel::ERROR, 'Error'));
+        $logs->add(new Log(LogLevel::CRITICAL, 'Critical'));
+        $logs->add(new Log(LogLevel::ALERT, 'Alert'));
+        $logs->add(new Log(LogLevel::EMERGENCY, 'Emergency'));
+
+        $assert = new Filter($logs);
+        /** @var Matcher $matcher */
+        $matcher = $assert->{$method}();
+        $actual = $matcher->getLogs();
+        self::assertEquals($expected, $actual);
+    }
+
+    public static function getProvider(): array
+    {
+        return [
+            ['getAll', [
+                new Log(LogLevel::DEBUG, 'Debug'),
+                new Log(LogLevel::INFO, 'Info'),
+                new Log(LogLevel::NOTICE, 'Notice'),
+                new Log(LogLevel::WARNING, 'Warning'),
+                new Log(LogLevel::ERROR, 'Error'),
+                new Log(LogLevel::CRITICAL, 'Critical'),
+                new Log(LogLevel::ALERT, 'Alert'),
+                new Log(LogLevel::EMERGENCY, 'Emergency'),
+            ]],
+            ['getDebug', [new Log(LogLevel::DEBUG, 'Debug')]],
+            ['getInfo', [new Log(LogLevel::INFO, 'Info')]],
+            ['getNotice', [new Log(LogLevel::NOTICE, 'Notice')]],
+            ['getWarning', [new Log(LogLevel::WARNING, 'Warning')]],
+            ['getError', [new Log(LogLevel::ERROR, 'Error')]],
+            ['getCritical', [new Log(LogLevel::CRITICAL, 'Critical')]],
+            ['getAlert', [new Log(LogLevel::ALERT, 'Alert')]],
+            ['getEmergency', [new Log(LogLevel::EMERGENCY, 'Emergency')]],
+        ];
+    }
+}
